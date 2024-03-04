2a15b7339df47fbf3f37a245a347a61fc08fb7cd
nurislam-td <ntasbolatov03@gmail.com>
2024-03-04 20:08:54 +0500

    Add  routes description

--- app/routers/api_v1/auth.py
+++ app/routers/api_v1/auth.py
@@ -18,7 +18,9 @@ from .dependencies import (
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
-@router.post("/users", status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/users", status_code=status.HTTP_201_CREATED, description="User registration"
+)
 async def register_user(
     user_data: UserCreate,
     uow: UOWDep,
@@ -26,7 +28,9 @@ async def register_user(
     return await user.create_user(user_data, uow=uow)
 
 
-@router.post("/users/login", status_code=status.HTTP_200_OK)
+@router.post(
+    "/users/login", status_code=status.HTTP_200_OK, description="Login to account"
+)
 async def login(
     response: Response,
     user_data: Annotated[UserDTO, Depends(validate_auth_data)],
@@ -37,30 +41,50 @@ async def login(
     return tokens
 
 
-@router.post("/users/email/send_code")
+@router.post(
+    "/users/email/send_code",
+    status_code=status.HTTP_200_OK,
+    description="Send verify code to email_in",
+)
 async def send_verify_message(email_in: EmailStr, uow: UOWDep) -> VerifyOut:
     return await email.send_verify_message(email=email_in, uow=uow)
 
 
-@router.post("/users/email/verify")
+@router.post(
+    "/users/email/verify",
+    status_code=status.HTTP_200_OK,
+    description="Check code valid",
+)
 async def verify_code(code: VerifyCodeCheck, uow: UOWDep) -> int:
     await email.check_code(code, uow=uow)
     return status.HTTP_200_OK
 
 
-@router.get("/users/me")
+@router.get(
+    "/users/me",
+    status_code=status.HTTP_200_OK,
+    description="Get token owner information",
+)
 async def get_me(user_data: UserDTO = Depends(get_current_user)) -> UserDTO:
     return user_data
 
 
-@router.post("/users/token/refresh")
+@router.post(
+    "/users/token/refresh",
+    status_code=status.HTTP_200_OK,
+    description="Refresh tokens via token(refresh_token)",
+)
 async def refresh_token(
     jwt_payload: Annotated[JWTPayload, Depends(validate_refresh_token)], uow: UOWDep
 ) -> TokenOut:
     return await user.refresh_tokens(jwt_payload=jwt_payload, uow=uow)
 
 
-@router.patch("/users/reset_password")
+@router.patch(
+    "/users/reset_password",
+    status_code=status.HTTP_200_OK,
+    description="User password reset func",
+)
 async def reset_password(
     update_data: UserResetPassword,
     uow: UOWDep,
@@ -68,7 +92,11 @@ async def reset_password(
     return await user.reset_password(update_data=update_data, uow=uow)
 
 
-@router.patch("/users")
+@router.patch(
+    "/users",
+    status_code=status.HTTP_200_OK,
+    description="Update token owner information",
+)
 async def update_user(
     update_data: UserUpdate,
     uow: UOWDep,
@@ -79,11 +107,19 @@ async def update_user(
     )
 
 
-@router.delete("/users/{user_id}")
-async def delete_user(user_id: UUID, uow: UOWDep):
+@router.delete(
+    "/users/{user_id}",
+    status_code=status.HTTP_200_OK,
+    description="Delete user",
+)
+async def delete_user(user_id: UUID, uow: UOWDep) -> None:
     await user.delete_user(user_id=user_id, uow=uow)
 
 
-@router.get("/users/")
+@router.get(
+    "/users/",
+    status_code=status.HTTP_200_OK,
+    description="Get all users from DB",
+)
 async def get_users(uow: UOWDep) -> list[UserDTO]:
     return await user.get_all(uow=uow)
