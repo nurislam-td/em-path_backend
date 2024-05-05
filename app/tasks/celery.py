5cd28d934c66c86605394aef76f09ffd09524c99
nuris <ntasbolatov03@gmail.com>
2024-05-05 20:28:09 +0500

    add clean verify code table task

--- app/tasks/celery.py
+++ app/tasks/celery.py
@@ -1,5 +1,13 @@
 from celery import Celery
+from celery.schedules import crontab
 
 from app.core.settings import settings
 
 celery = Celery("tasks", broker=settings.redis.url, include=["app.tasks.tasks"])
+
+celery.conf.beat_schedule = {
+    "run-every-midnight": {
+        "task": "clean_verify_code_table",
+        "schedule": crontab(minute="0", hour="0"),  # run every 00:00 UTC+0
+    }
+}
--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -1,19 +1,22 @@
-from sqlalchemy import insert, update
+from datetime import UTC, datetime, timedelta
+
+from celery import shared_task
+from sqlalchemy import delete, insert, update
 
 from app.core.database import engine
+from app.core.settings import settings
 from app.models.auth import VerifyCode
 from app.service import email, secure
-from app.tasks.celery import celery
 
 
-@celery.task
+@shared_task
 def deactivate_verify_code(email):
     with engine.begin() as conn:
         table = VerifyCode.__table__
         conn.execute(update(table).values(is_active=False).filter_by(email=email))
 
 
-@celery.task
+@shared_task
 def send_verify_message(email_in: str):
     email_code = secure.generate_random_num()
     message = f"This your verification code {email_code}"
@@ -24,3 +27,13 @@ def send_verify_message(email_in: str):
         conn.execute(
             insert(table).values(email=email_in, code=email_code, is_active=True)
         )
+
+
+@shared_task(name="clean_verify_code_table")
+def clean_verify_code_table():
+    with engine.begin() as conn:
+        table = VerifyCode.__table__
+        time_filter = datetime.now(UTC) - timedelta(
+            minutes=settings.auth_config.verification_code_expire
+        )
+        conn.execute(delete(table).where(table.c.created_at < time_filter))
